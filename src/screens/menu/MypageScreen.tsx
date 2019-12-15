a4133dff78e135f5ec1cb490d0be76fda0b7e394
Maya <melokitty.1003@gmail.com>
2019-12-16 01:18:05 +0900

    プロフィール編集の所にimagePicker追加

--- src/screens/menu/MypageScreen.tsx
+++ src/screens/menu/MypageScreen.tsx
@@ -1,33 +1,17 @@
 import * as React from 'react';
 import {View, Text, Image, StyleSheet} from "react-native";
 import CommonButton from "../../components/parts/common/CommonButton";
-import {ImageSelecter} from "../../common/image/ImageSelecter";
 import {ImageInfo} from "expo-image-picker/build/ImagePicker.types";
 import {NavigationScreenProp} from "react-navigation";
 
-type State = {
-    localImage: ImageInfo | null
-}
 
 type Props = {
     navigation: NavigationScreenProp<{}>
 }
 
-const style = StyleSheet.create({
-    imageStyle: {
-        width: 100,
-        height: 100
-    }
-})
 
-export default class MypageScreen extends React.Component <Props, State> {
 
-    public constructor(props) {
-        super(props);
-        this.state = {
-            localImage: null
-        }
-    }
+export default class MypageScreen extends React.Component <Props> {
 
 
     public componentDidMount() {
@@ -37,18 +21,6 @@ export default class MypageScreen extends React.Component <Props, State> {
     }
 
 
-
-    //画像追加ボタンが押された
-    private uploadImage = () => {
-        ImageSelecter.execute()
-            .then(result => {
-                if (result.cancelled) return;
-                this.setState({
-                    localImage: result as ImageInfo
-                });
-                // this.props.navigation.state.params.localImage = this.state.localImage
-            });
-    }
 
     public goToProfileScreen = () => {
         this.props.navigation.navigate('ProfileEdit', {
@@ -60,12 +32,7 @@ export default class MypageScreen extends React.Component <Props, State> {
         return(
             <View>
                 <Text>写真をアップロード</Text>
-               <CommonButton title="写真追加"　onPress={this.uploadImage}/>
                 <CommonButton title="プロフィール編集"　onPress={() => this.props.navigation.state.params.goToProfileScreen()}/>
-                {this.state.localImage ?
-                    <Image source={{uri: this.state.localImage.uri}} style={style.imageStyle}/>
-                    : null
-                }
             </View>
         )
     }
--- src/screens/menu/ProfileEditScreen.tsx
+++ src/screens/menu/ProfileEditScreen.tsx
@@ -1,16 +1,23 @@
 import * as React from 'react';
-import {View, Text,TouchableHighlight, Button, StyleSheet, TextInput} from "react-native"
+import {View, Text, Image,TouchableHighlight, Button, StyleSheet, TextInput} from "react-native"
 import PostTile from "../../components/parts/common/PostTile";
 import * as firebase from "firebase";
 import { NavigationScreenProp} from "react-navigation"
 import Color from "../../common/Color";
-require('firebase/firestore')
-
+import {ImageSelecter} from "../../common/image/ImageSelecter";
+import CommonButton from "../../components/parts/common/CommonButton";
+import {ImageInfo} from "expo-image-picker/build/ImagePicker.types";
+import {url} from "inspector";
+require('firebase/firestore');
 
 
 const style = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    imageStyle: {
+        width: 100,
+        height: 100
     }
 })
 
@@ -19,61 +26,63 @@ type Props = {
 }
 
 type State = {
-    postContent: string
+    postContent: string,
+    localImage: ImageInfo | null
 }
+
 
 export default class ProfileEditScreen extends React.Component <Props, State> {
 
     public constructor(props) {
         super(props);
         this.state = {
-            postContent: ""
+            postContent: "",
+            localImage: null
         }
     }
 
 
-    public handleSubmit = () => {
-        const db = firebase.firestore()
-        db.collection('posts').add({
-            content: this.state.postContent,
-            createdAt: new Date().toLocaleString("ja")
-        })
-            .then(() => {
-                console.log('succes!')
-                console.log(this.props.navigation.state.params.refresh());
-                // 好きな場所でこれを呼び出すと親画面の更新が行われる。
-                this.props.navigation.state.params.refresh();
-                this.props.navigation.goBack()
-            })
-            .catch((error) => {
-                console.log('error')
+    // public handleSubmit = () => {
+    //     const db = firebase.firestore()
+    //     db.collection('posts').add({
+    //         content: this.state.postContent,
+    //         createdAt: new Date().toLocaleString("ja")
+    //     })
+    //         .then(() => {
+    //             console.log('succes!')
+    //             console.log(this.props.navigation.state.params.refresh());
+    //             // 好きな場所でこれを呼び出すと親画面の更新が行われる。
+    //             this.props.navigation.state.params.refresh();
+    //             this.props.navigation.goBack()
+    //         })
+    //         .catch((error) => {
+    //             console.log('error')
+    //         })
+    // }
+
+    public addPhotoPress = () => {
+        ImageSelecter.execute()
+            .then(result => {
+                if (result.cancelled) return;
+                this.setState({
+                    localImage: result as ImageInfo
+                })
             })
     }
-
 
     public render() {
 
         return(
             <View style={style.container}>
                 <Text>画像をアップしましょう！！</Text>
-                <TextInput
-                    onChangeText={(text) => this.setState({postContent: text})}
-                    value={this.state.postContent}
-                    multiline={true}
-                    style={{borderColor: Color.black,
-                        borderWidth: 1,
-                        flex: 1,
-                        flexDirection: 'row',
-                        alignItems: "center",
-                        marginTop: 16,
-                        marginLeft: 20,
-                        marginRight: 20,
-                    }}/>
-                <Button
-                    title="投稿する"
-                    style={{marginRight: 20}}
-                    onPress={() => this.handleSubmit()}
+                <CommonButton
+                    title="写真追加"
+                    onPress={() => this.addPhotoPress()}
                 />
+                {this.state.localImage ?
+                    <Image source={{uri: this.state.localImage.uri}} style={style.imageStyle}/>
+                    : null
+                }
             </View>
         )
     }
