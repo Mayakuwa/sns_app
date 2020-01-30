55223882267732b4f6bf84b8ab0d4b7be0dac326
Maya <melokitty.1003@gmail.com>
2020-01-30 19:34:10 +0900

    記事取得（特定の記事を取れるようにする）

--- src/screens/menu/ArticleScreen.tsx
+++ src/screens/menu/ArticleScreen.tsx
@@ -28,15 +28,52 @@ export default class ArticleScreen extends React.Component <State, Props> {
         return this.fetch()
     }
 
+    // private fetch = () => {
+    //     return fetch("https://jp.techcrunch.com/feed/")
+    //         .then((response) => response.text())
+    //         .then((responseData) => rssParser.parse(responseData))
+    //         .then((rss)=> {
+    //             // rss.items[0].links[0].urlでurlが取れる
+    //            this.setState({items: rss.items})
+    //         })
+    // }
+
     private fetch = () => {
-        return fetch("https://jp.techcrunch.com/feed/")
-            .then((response) => response.text())
-            .then((responseData) => rssParser.parse(responseData))
-            .then((rss)=> {
-                // rss.items[0].links[0].urlでurlが取れる
-               this.setState({items: rss.items})
+        fetch("https://microsoft-azure-bing-news-search-v1.p.rapidapi.com/search?count=3&offset=0&mkt=ja-jp&safeSearch=Off&q=%E4%BB%8B%E8%AD%B7", {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "microsoft-azure-bing-news-search-v1.p.rapidapi.com",
+                "x-rapidapi-key": "9ae8c0af1cmshcb1d0c2d49c5985p1bc3bbjsnfaf1242e8cee",
+            }
+        })
+            .then(response => {
+                return response.json()
             })
+            .then((data) => {
+                console.warn(data.value)
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        // return fetch("https://microsoft-azure-bing-news-search-v1.p.rapidapi.com/search?count=5&q=%25E4%25BB%258B%25E8%25AD%25B7", {
+        //     "method": "GET",
+        //     "headers": {
+        //         "x-rapidapi-host": "microsoft-azure-bing-news-search-v1.p.rapidapi.com",
+        //         "x-rapidapi-key": "48ad87710fmsh930fac3c8021acep19e621jsned186a5c9f5e"
+        //     }
+        // })
+        //     .then(response => {
+        //         console.warn(response);
+        //         return response.json()
+        //     })
+        //     .then((data) => {
+        //         console.warn(data.value);
+        //     })
+        //     .catch(err => {
+        //         console.log(err);
+        //     });
     }
+
 
 
     private onWebView = (url) => {
@@ -49,12 +86,11 @@ export default class ArticleScreen extends React.Component <State, Props> {
     public render() {
        const articles = this.state.items.map(item =>(
            <ListItem onPress={() => this.onWebView(item.links[0].url)}>
-               {console.warn(item.links[0].url)}
                <Left>
                    <Thumbnail square source={{ uri: 'https://www.joint-kaigo.com/1/_src/58756/img20191207081956811162.jpg?v=1575675517779' }} />
                </Left>
                <Body>
-                   <Text numberOfLines={2}>{item.title}</Text>
+                   <Text numberOfLines={2}>{item.name}</Text>
                </Body>
                <Right>
                    <Button transparent>
